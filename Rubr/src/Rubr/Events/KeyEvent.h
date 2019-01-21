973d215a7581992203dda632467bad10ba1981dd
Scott Moisik <scott.moisik@ntu.edu.sg>
2019-01-21 19:45:34 +0800

    ImGui event handling

--- Rubr/src/Rubr/Events/KeyEvent.h
+++ Rubr/src/Rubr/Events/KeyEvent.h
@@ -44,4 +44,17 @@ namespace Rubr {
 
 		EVENT_CLASS_TYPE(KeyReleased)
 	};
+
+	class RUBR_API KeyTypedEvent : public KeyEvent {
+	public:
+		KeyTypedEvent(int keycode) : KeyEvent(keycode) {}
+
+		std::string ToString() const override {
+			std::stringstream ss;
+			ss << "KeyTypedEvent: " << m_KeyCode;
+			return ss.str();
+		}
+
+		EVENT_CLASS_TYPE(KeyTyped)
+	};
 }
--- Rubr/src/Rubr/ImGui/ImGuiLayer.h
+++ Rubr/src/Rubr/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Rubr/Layer.h"
+#include "Rubr/Events/AppEvents.h"
+#include "Rubr/Events/KeyEvent.h"
+#include "Rubr/Events/MouseEvent.h"
 
 namespace Rubr {
 	class RUBR_API ImGuiLayer : public Layer {
@@ -12,6 +15,15 @@ namespace Rubr {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
  	};
